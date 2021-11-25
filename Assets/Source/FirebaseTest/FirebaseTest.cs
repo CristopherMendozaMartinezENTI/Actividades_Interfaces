8ace46d18b05c3adde729ae82c4d6f449f5b7afc
Cristopher Mendoza <thekrisxp1@gmail.com>
2021-11-25 17:49:59 +0100

    Minor Fixes

--- Assets/Source/FirebaseTest/FirebaseTest.cs
+++ Assets/Source/FirebaseTest/FirebaseTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
+public class FirebaseTest : MonoBehaviour
 {
     private Firebase.FirebaseApp app;
     // Start is called before the first frame update
